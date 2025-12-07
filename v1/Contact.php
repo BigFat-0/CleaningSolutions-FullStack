007670078b530e343cb2ee3a9515467d2824a254
unknown <cdabrowski071@gmail.com>
2025-12-07 13:38:38 +0000

    v1.9564

--- v1/Contact.php
+++ v1/Contact.php
@@ -168,7 +168,7 @@
                     </div>
                 </div>
                 
-                <div class="mbr-section-btn pt-3 text-center"><a class="btn btn-black-outline display-4" href="index.php#form02-6">Enquire Here</a></div>
+                <div class="mbr-section-btn pt-3 text-center"><a class="btn btn-black-outline display-4" href="get_quote.php">Enquire Here</a></div>
                 
                 
                 
@@ -215,7 +215,7 @@
             
         </div>
     </div>
-</section><section class="display-7" style="padding: 0;align-items: center;justify-content: center;flex-wrap: wrap;    align-content: center;display: flex;position: relative;height: 4rem;"><a href="https://mobiri.se/" style="flex: 1 1;height: 4rem;position: absolute;width: 100%;z-index: 1;"><img alt="" style="height: 4rem;" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="></a><p style="margin: 0;text-align: center;" class="display-7">&#8204;</p><a style="z-index:1" href="https://mobirise.com/builder/ai-website-builder.html">Free AI Website Software</a></section><script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>  <script src="assets/smoothscroll/smooth-scroll.js"></script>  <script src="assets/ytplayer/index.js"></script>  <script src="assets/dropdown/js/navbar-dropdown.js"></script>  <script src="assets/theme/js/script.js"></script>  
+</section><script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>  <script src="assets/smoothscroll/smooth-scroll.js"></script>  <script src="assets/ytplayer/index.js"></script>  <script src="assets/dropdown/js/navbar-dropdown.js"></script>  <script src="assets/theme/js/script.js"></script>  
   
   
   <input name="animation" type="hidden">
